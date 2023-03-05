b78b0ee2036a231ad7e7871a38c99d648967a592
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-05 12:23:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello World</h2>
+      <label id="name" htmlFor="name">
+        Name:
+      </label>
+      <input type="text" id="name"></input>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Wikipedia:Hauptseite"
+      >
+        Link
+      </a>
+    </article>
+  );
 }
